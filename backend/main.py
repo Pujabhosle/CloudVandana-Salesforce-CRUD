eb9ac93a354d575f2e70c2293c2c3a4e86f9f27e
Pooja Bhosale <12345678+Pujabhosle@users.noreply.github.com>
2026-08-16 10:26:04 +0530

    Allow deployed frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -24,10 +24,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://cloudvandana-salesforce-crud-1-23dd.onrender.com",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5174",
-        "http://127.0.0.1:5174",
+        "http://127.0.0.1:5174"
     ],
     allow_credentials=True,
     allow_methods=["*"],
